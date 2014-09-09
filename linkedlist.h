0f229cf3993d3faebf0300d2de697bea62b7fda8
admix <admix.snurnikov@gmail.com>
2014-09-09 15:20:05 -0400

    Added remove nodes

--- linkedlist.h
+++ linkedlist.h
@@ -32,6 +32,12 @@ public:
   //return address of this new node
   Node<TYPE>* insertAtBack(const TYPE& data);
 
+  //remove a node from front
+  Node<TYPE>* removeAtFront();
+
+  //remove a node from back
+  Node<TYPE>* removeAtBack();
+
   //prints the linked list assuming that the TYPE can
   //be ouputed using standart << operator
   void print() const;
@@ -72,6 +78,37 @@ Node<TYPE>* SinglyLinked<TYPE>::insertAtBack(const TYPE& data) {
   return nn;
 }
 
+//remove a node from front
+template <class TYPE>
+void removeAtFront() {
+  if(first_) {
+    Node<TYPE>* tmp = first_;
+    first_ = first_->next_; //makes first_ points to the second node
+    delete tmp;
+  }
+}
+
+//remove a node from back
+template <class TYPE>
+void removeAtBack() {
+  if(first_) {
+    Node<TYPE>* curr = first_;
+    Node<TYPE>* prev = NULL;
+    while(curr->next_) {
+      prev = curr;
+      curr=curr->next_;
+    }
+    if(prev != NULL) {
+      prev->next_ = NULL;
+    }
+    else {
+      first_ = NULL;
+    }
+    delete curr;
+  }
+}
+
+//prints Nodes
 template <class TYPE>
 void SinglyLinked<TYPE>::print() const {
   Node<TYPE>* curr = first_;
@@ -80,3 +117,8 @@ void SinglyLinked<TYPE>::print() const {
     curr = curr->next_;
   }
 }
+
+
+//WHAT IS THE RUNTIME OF removes
+
+//modify code to add last pointer to remove and calculate runtime with it
