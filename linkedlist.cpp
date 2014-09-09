ceb2425bafa5b0ad709d8619b4043d6912ddf44a
admix <admix.snurnikov@gmail.com>
2014-09-09 14:06:52 -0400

    Initial commit

---
+++ linkedlist.cpp
@@ -0,0 +1,16 @@
+//Linked list
+
+template <TYPE>
+struct Node {
+  TYPE data_;
+  Node<TYPE>* next_;
+  Node(const TYPE& data, Node*<TYPE>* n = NULL) {
+    
+  }
+
+};
+
+template <TYPE>
+class SinglyLinked {
+
+}
